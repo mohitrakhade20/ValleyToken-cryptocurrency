0f6b35a5cfa8650d45ef6559b5480c8e29d7d24e
Mohit <mohitrakhade20@gmail.com>
2020-06-01 17:35:44 +0530

    Third: Delegated Transfer

--- contracts/ValleyToken.sol
+++ contracts/ValleyToken.sol
@@ -9,7 +9,16 @@ contract ValleyToken {
 
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
 
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
+
     mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+
+    // allowance
 
     constructor(uint256 _initialSupply) public {
         balanceOf[msg.sender] = _initialSupply;
@@ -23,15 +32,44 @@ contract ValleyToken {
     {
         // exception if acc doesn't have enough
         require(balanceOf[msg.sender] >= _value, "Sender not authorized.");
-        // Transfer balance
+
         balanceOf[msg.sender] -= _value;
         balanceOf[_to] += _value;
 
         emit Transfer(msg.sender, _to, _value);
 
         return true;
+    }
 
-        // return a boolean
-        // tarnsfer event
+    //approve
+    function approve(address _spender, uint256 _value)
+        public
+        returns (bool success)
+    {
+        allowance[msg.sender][_spender] = _value;
+
+        emit Approval(msg.sender, _spender, _value);
+
+        return true;
+    }
+
+    //transfrom
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    ) public returns (bool success) {
+        require(_value <= balanceOf[_from], "this is authorized");
+
+        require(_value <= allowance[_from][msg.sender], "err");
+
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+
+        allowance[_from][msg.sender] -= _value;
+
+        emit Transfer(_from, _to, _value);
+
+        return true;
     }
 }
